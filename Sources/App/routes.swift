81e64a6bda7613f96e104a2b4653513396f19d06
Alberto Alegre <tyrion72@gmail.com>
2025-04-09 13:58:14 +0200

    [CHANGE] Change well Known to Routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,6 +2,14 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
+    app.get(".well-known", "apple-app-site-association") { req -> Response in
+        let path = req.application.directory.publicDirectory + ".well-known/apple-app-site-association"
+        let data = try Data(contentsOf: URL(fileURLWithPath: path))
+        var headers = HTTPHeaders()
+        headers.add(name: .contentType, value: "application/json")
+        return Response(status: .ok, headers: headers, body: .init(data: data))
+    }
+    
     try app.register(collection: AuthController())
     
     //let protectedRoutes = app.grouped(JWTMiddleware())
